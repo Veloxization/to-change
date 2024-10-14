99a0d3b98ccba1694384cba09676176d8c7d07bb
Veloxization <github@velo.anonaddy.com>
2024-10-14 14:40:51 +0300

    Fancy cards display, shuffling and peeking added

--- modules/cards/sheet/tc-cards-sheet.js
+++ modules/cards/sheet/tc-cards-sheet.js
@@ -1,10 +1,17 @@
-export default class TCCardsSheet extends DocumentSheet {
+export default class TCCardsSheet extends CardsConfig {
+    constructor(cards, options) {
+        super(cards, options);
+
+        this.cards = cards;
+        options.submitOnChange = true;
+    }
+
     get template() {
-        return `systems/to-change/templates/cards/tc-${this.document.type}-sheet.hbs`
+        return `systems/to-change/templates/cards/tc-${this.document.type}-sheet.hbs`;
     }
 
     static get defaultOptions() {
-        const defaultWidth = 840;
+        const defaultWidth = 870;
         const defaultHeight = 700;
         return foundry.utils.mergeObject(super.defaultOptions, {
             width: defaultWidth,
@@ -17,6 +24,39 @@ export default class TCCardsSheet extends DocumentSheet {
                     initial: "cards-list"
                 }
             ]
+        });
+    }
+
+    getData(options={}) {
+        return {
+            cardsSorted: this.getShuffledDeck(),
+            document: this.document
+        };
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        html.on('click', '#peek-button', (ev) => {
+            this.peekCards();
+            this.render();
+        });
+
+        html.on('click', '#shuffle-button', (ev) => {
+            this.cards.shuffle();
+            this.render();
+        });
+    }
+
+    getShuffledDeck() {
+        var cards = this.cards.cards.contents;
+        return cards.toSorted((a, b) => this.cards.sortShuffled(a, b));
+    }
+
+    peekCards() {
+        this.document.system.peeking = true;
+        this.getShuffledDeck().forEach(card => {
+            card.flip();
         });
     }
 }
